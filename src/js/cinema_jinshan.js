8a1216d8d8bf3c46d89ed56912cd70b34ac34299
Mis min <min@qq.com>
2017-09-10 17:04:59 +0800

    影院详情

---
+++ src/js/cinema_jinshan.js
@@ -0,0 +1,11 @@
+/**
+ * Author Administrator.
+ * des
+ * Date 2017/9/8.
+ * Time 16:10.
+ */
+$(document).ready(function(){
+    $("#navbuttom").click(function(){
+        $("#navbar-collapse").toggle();
+    });
+});
