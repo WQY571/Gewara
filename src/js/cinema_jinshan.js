c8e37af45d206180ae1954f8c235eb052843da69
Mis min <min@qq.com>
2017-09-12 01:43:38 +0800

    Merge branch 'master' of E:\togogo\webApp\Gewara with conflicts.

--- src/js/cinema_jinshan.js
+++ src/js/cinema_jinshan.js
@@ -8,4 +8,18 @@ $(document).ready(function(){
     $("#navbuttom").click(function(){
         $("#navbar-collapse").toggle();
     });
+    $('.owl-carousel').owlCarousel({
+        items: 4 ,
+        loop: true,
+        margin: 1,
+        merge: true,
+        responsive: {
+            678: {
+                mergeFit: true
+            },
+            1000: {
+                mergeFit: false
+            }
+        }
+    });
 });
