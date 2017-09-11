a9f2deae351036df43b7b94860a817e738b6df2e
froz <1120735075@qq.com>
2017-09-11 17:55:07 +0800

    影院页更新

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,8 @@
  */
 var app = angular.module('gewara', [
     'ngRoute',
-    'movies.hot'
+    'movies.hot',
+    'cinema'
 ]);
 
 app.config(['$routeProvider', function ($routeProvider) {
---
+++ src/js/cinema.js
@@ -0,0 +1,7 @@
+/**
+ * Created by 冯智昌 on 2017/9/10/010.
+ */
+var app = angular.module('cinema', []);
+app.controller('cinemaController', ["$scope", "$http", function ($scope, $http) {
+
+}]);
