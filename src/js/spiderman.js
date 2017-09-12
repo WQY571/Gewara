277bb9392ff9fccd719e0276707c67d7018c4d2d
wqy <546758863@qq.com>
2017-09-12 16:43:40 +0800

    spider-man

--- src/js/spiderman.js
+++ src/js/spiderman.js
@@ -10,4 +10,23 @@ function read() {
     $("#detailsimg").click(function () {
         window.location.href="hero-coming.html"
     })
+
+    $("#unflod").click(function () {
+
+        var helloDivObj = $("#introduce");
+        var buttonObj = $("#unflod");
+        var val = buttonObj.attr("value");
+        if(val=="展开"){
+            helloDivObj.addClass("open");
+            buttonObj.attr("value","收起");
+        }else{
+            helloDivObj.removeClass("open");
+            buttonObj.attr("value","展开");
+        }
+       /* $("#middle1").addClass("open");
+       /!* $("#unflod").css("text","收起").addClass("close");*!/*/
+    })
+
+
+
 }
